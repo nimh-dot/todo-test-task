0dc82458aecf11a5042458032eabaedc17e70ded
Vyachaslau <sokol.v083@gmail.com>
2024-12-12 01:14:41 +0300

    типизация AddTodo компонента

--- todo-test-task/src/components/AddTodo/AddTodo.tsx
+++ todo-test-task/src/components/AddTodo/AddTodo.tsx
@@ -1,32 +1,35 @@
 import { useRef, useState } from 'react';
 import styles from './AddTodo.module.css';
 
+interface IAddTodoProps {
+    addTodo: (newTodo: string) => void;
+}
 
-const AddTodo = ({updateTodos}) => {
+const AddTodo: React.FC<IAddTodoProps> = ({addTodo}) => {
     const inputRef = useRef<HTMLInputElement>(null);
-    const [newTask, setNewTask] = useState('');
+    const [newTask, setNewTask] = useState<string>('');
 
     const handleKeyDown: React.KeyboardEventHandler<HTMLInputElement> = (e) => {
-        if (e.key === 'Enter' && newTask.length) { //trim length
-            updateTodos(newTask);
+        if (e.key === 'Enter' && newTask.trim().length) {
+            addTodo(newTask);
             setNewTask('');
         } 
-    }
+    };
 
     const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
         setNewTask(e.target.value);
-    }
+    };
 
     return (
         <input className={styles.input}
-        value={newTask} 
-        type='text'
-        placeholder='What needs to be done?'
-        onChange={handleChange}
-        onKeyDown={handleKeyDown}
-        ref={inputRef}
-        autoFocus={true}
-      />
+            value={newTask} 
+            type='text'
+            placeholder='What needs to be done?'
+            onChange={handleChange}
+            onKeyDown={handleKeyDown}
+            ref={inputRef}
+            autoFocus={true}
+        />
     )
 }
 
