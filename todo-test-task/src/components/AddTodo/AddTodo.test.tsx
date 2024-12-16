cb53510ffa04f4c140ba1e3c431f661cba8d86e6
Vyachaslau <sokol.v083@gmail.com>
2024-12-16 13:13:55 +0300

    тесты для компоненты ввода новой задачи

---
+++ todo-test-task/src/components/AddTodo/AddTodo.test.tsx
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import AddTodo from './AddTodo';
+
+const addTodo = (newTodo: string) => {};
+
+describe('AddTodo', () => {
+  it('renders the AddTodo component', () => {
+    render(<AddTodo addTodo={addTodo}/>)
+    screen.debug()
+  })
+
+  it('input field is present', () => {
+    render(<AddTodo addTodo={addTodo}/>)
+    expect(screen.getByRole('textbox')).toBeInTheDocument()
+  })
+
+  it('input field placeholder correct', () => {
+    render(<AddTodo addTodo={addTodo}/>)
+    expect(screen.getByPlaceholderText(/What needs to be done?/i)).toBeInTheDocument()
+  })
+
+  it('component respond on keyboard input', async () => {
+    render(<AddTodo addTodo={addTodo}/>)
+
+    await userEvent.type(screen.getByRole('textbox'), 'new task')
+    expect(screen.queryByDisplayValue('new task')).toBeInTheDocument()
+  })
+  
+  it('input is in the focus after render', () => {
+    render(<AddTodo addTodo={addTodo}/>)
+    expect(screen.getByRole('textbox')).toHaveFocus()
+  })
+})
